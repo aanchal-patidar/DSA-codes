e9b908f77c26af310f9f79c00cc9db1ac654a031
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-08 12:29:10 +0530

    Update relational_logical_2.cpp

--- relational_logical_2.cpp
+++ relational_logical_2.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : relational_logical_2.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
