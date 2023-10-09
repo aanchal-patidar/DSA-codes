b488028c0142187cbfc8d393cb75a390f668854e
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-09 18:16:25 +0530

    Update print_1_to_n.cpp

--- while_loop_4/print_1_to_n/src/print_1_to_n.cpp
+++ while_loop_4/print_1_to_n/src/print_1_to_n.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : print_1_to_n.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
