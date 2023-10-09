2333016585f30fdd9cf8602292f528d3ebc32711
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-09 18:21:16 +0530

    Update check_positive_negative.cpp

--- check_positive_negative.cpp
+++ check_positive_negative.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : check_positive_negative.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
