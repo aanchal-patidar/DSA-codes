3c13061b8ac5e1771cb5de0d199a28880a3a61e0
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-10 17:21:53 +0530

    Update count_number_triangle.cpp

--- patterns_while_5/count_number_triangle/src/count_number_triangle.cpp
+++ patterns_while_5/count_number_triangle/src/count_number_triangle.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : count_number_triangle.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
