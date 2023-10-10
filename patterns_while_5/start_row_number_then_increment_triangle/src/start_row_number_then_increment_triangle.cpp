b3befadc3284301780284f7da2be01bacd61f5a8
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-10 17:54:08 +0530

    Update start_row_number_then_increment_triangle.cpp

--- patterns_while_5/start_row_number_then_increment_triangle/src/start_row_number_then_increment_triangle.cpp
+++ patterns_while_5/start_row_number_then_increment_triangle/src/start_row_number_then_increment_triangle.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : start_row_number_then_increment_triangle.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
