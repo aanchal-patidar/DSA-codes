de0aa47ee0a737fc40f7017fc414e1729ea9c250
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-10 10:37:18 +0530

    Update column_alphabet_square.cpp

--- patterns_while_5/column_alphabet_square/src/column_alphabet_square.cpp
+++ patterns_while_5/column_alphabet_square/src/column_alphabet_square.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : column_alphabet_square.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
