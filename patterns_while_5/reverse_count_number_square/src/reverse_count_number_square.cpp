5a8f1318ae3d3cd934689c85234158ae6639f38d
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-10 17:30:21 +0530

    Update reverse_count_number_square.cpp

--- patterns_while_5/reverse_count_number_square/src/reverse_count_number_square.cpp
+++ patterns_while_5/reverse_count_number_square/src/reverse_count_number_square.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : reverse_count_number_square.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
@@ -11,24 +10,22 @@ using namespace std;
 
 int main() {
 	int n;
-				cout << "Enter a number:";
-				cin>> n;
+	cout << "Enter a number:";
+	cin>> n;
 
-				int i = 1;
-				int count = 1;
-				while(i <= n){
-					int j = 1;
-					int val = n-j+6;
-					while(j <= n){
-
-
-						cout <<val << " ";
-						val= val -1;
-						count = count+1;
-						j = j+1;
-					}
-					cout << endl;
-					i = i+1;
-				}
+	int i = 1;
+	int count = 1;
+	while(i <= n){
+		int j = 1;
+		int val = n-j+6;
+		while(j <= n){
+			cout <<val << " ";
+			val= val -1;
+			count = count+1;
+			j = j+1;
+		}
+		cout << endl;
+		i = i+1;
+	}
 	return 0;
 }
