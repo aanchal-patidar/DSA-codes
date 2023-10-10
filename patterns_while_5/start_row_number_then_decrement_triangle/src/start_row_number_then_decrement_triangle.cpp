3ffdcad9f961c8ab6bbd585c9e17cc9a9f25ba8c
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-10 17:52:51 +0530

    Update start_row_number_then_decrement_triangle.cpp

--- patterns_while_5/start_row_number_then_decrement_triangle/src/start_row_number_then_decrement_triangle.cpp
+++ patterns_while_5/start_row_number_then_decrement_triangle/src/start_row_number_then_decrement_triangle.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : start_row_number_then_decrement_triangle.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
@@ -11,21 +10,21 @@ using namespace std;
 
 int main() {
 	int n;
-			cout << "Enter a number : " ;
-			cin >> n;
+	cout << "Enter a number : " ;
+	cin >> n;
 
-			int i = 1;
-			while(i <= n){
-				int j = 1;
-				int  row = i;
-				while(j <=i){
-					cout << row << " ";
-					row = row - 1;
-					//cout << i +j -1 << " ";
-					j = j+ 1;
-				}
-				cout << endl;
-				i = i +1;
-			}
+	int i = 1;
+	while(i <= n){
+		int j = 1;
+		int  row = i;
+		while(j <=i){
+			cout << row << " ";
+			row = row - 1;
+			//cout << i +j -1 << " ";
+			j = j+ 1;
+		}
+		cout << endl;
+		i = i +1;
+	}
 	return 0;
 }
