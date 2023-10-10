e25e6c93c20ffb714e09f643e5f6009623e6c42e
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-10 17:35:03 +0530

    Update row_number_pattern_square.cpp

--- patterns_while_5/row_number_pattern_square/src/row_number_pattern_square.cpp
+++ patterns_while_5/row_number_pattern_square/src/row_number_pattern_square.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : row_number_pattern_square.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
