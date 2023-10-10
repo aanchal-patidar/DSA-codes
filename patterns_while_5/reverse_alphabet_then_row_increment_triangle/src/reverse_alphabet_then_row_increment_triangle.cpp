52f2ae10f68ccecbe722a83d02dd5bea200eef0c
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-10 17:25:13 +0530

    Update reverse_alphabet_then_row_increment_triangle.cpp

--- patterns_while_5/reverse_alphabet_then_row_increment_triangle/src/reverse_alphabet_then_row_increment_triangle.cpp
+++ patterns_while_5/reverse_alphabet_then_row_increment_triangle/src/reverse_alphabet_then_row_increment_triangle.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : reverse_alphabet_then_row_increment_triangle.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
@@ -10,21 +9,21 @@
 using namespace std;
 
 int main() {
-					int n;
-					cout << "Enter a number:";
-					cin>> n;
+	int n;
+	cout << "Enter a number:";
+	cin>> n;
 
-					int i = 1;
-					while(i <= n){
-						int j = 1;
-						char ch = 'A'+n-i;
-						while(j <= i){
-							cout <<ch << " ";
-							ch= ch +1;
-							j = j+1;
-						}
-						cout << endl;
-						i = i+1;
-					}
+	int i = 1;
+	while(i <= n){
+		int j = 1;
+		char ch = 'A'+n-i;
+		while(j <= i){
+			cout <<ch << " ";
+			ch= ch +1;
+			j = j+1;
+		}
+	cout << endl;
+	i = i+1;
+	}
 	return 0;
 }
