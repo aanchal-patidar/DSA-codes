6a70375839d5440896598da76c25f60a1e889450
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-10 17:43:29 +0530

    Update space_row_number_increment_triangle.cpp

--- patterns_while_5/space_row_number_increment_triangle/src/space_row_number_increment_triangle.cpp
+++ patterns_while_5/space_row_number_increment_triangle/src/space_row_number_increment_triangle.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : space_row_number_increment_triangle.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
@@ -11,23 +10,23 @@ using namespace std;
 
 int main() {
 	int n;
-		cout << "Enter a number : ";
-		cin >> n;
+	cout << "Enter a number : ";
+	cin >> n;
 
-		int i = 1;
-		while(i <= n){
-			int space = 1;
-			while(space <= n - i){
-				cout << " " ;
-				space = space + 1;
-			}
-			int j = 1;
-			while(j<= i){
-				cout << i ;
-				j = j + 1;
-			}
-			cout << endl;
-			i = i +1;
+	int i = 1;
+	while(i <= n){
+		int space = 1;
+		while(space <= n - i){
+			cout << " " ;
+			space = space + 1;
+		}
+		int j = 1;
+		while(j<= i){
+			cout << i ;
+			j = j + 1;
+		}
+		cout << endl;
+		i = i +1;
 		}
 	return 0;
 }
