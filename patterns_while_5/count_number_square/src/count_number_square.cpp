7cdd0d7e77bc9e8ac0992c12207c647c0161908a
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-10 17:20:26 +0530

    Update count_number_square.cpp

--- patterns_while_5/count_number_square/src/count_number_square.cpp
+++ patterns_while_5/count_number_square/src/count_number_square.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : count_number_square.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
@@ -10,26 +9,24 @@
 using namespace std;
 
 int main() {
-			int n;
-			cout << "Enter a number:";
-			cin>> n;
+	int n;
+	cout << "Enter a number:";
+	cin>> n;
 
-			int i = 1;
-			int count = 1;
+	int i = 1;
+	int count = 1;
 
-			while(i <= n){
-				int j = 1;
+	while(i <= n){
+	int j = 1;
 
-				while(j <= n){
-
-
-					cout <<count << " ";
-					count = count + 1;
-					j = j+1;
-				}
-				cout << endl;
-				i = i+1;
-			}
+	while(j <= n){
+		cout <<count << " ";
+		count = count + 1;
+		j = j+1;
+	}
+	cout << endl;
+	i = i+1;
+	}
 
 	return 0;
 }
