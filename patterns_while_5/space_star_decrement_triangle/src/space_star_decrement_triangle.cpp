765c59b91e34dddd385fd8ef31f430b32c095ee4
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-10 17:46:15 +0530

    Update space_star_decrement_triangle.cpp

--- patterns_while_5/space_star_decrement_triangle/src/space_star_decrement_triangle.cpp
+++ patterns_while_5/space_star_decrement_triangle/src/space_star_decrement_triangle.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : space_star_decrement_triangle.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
