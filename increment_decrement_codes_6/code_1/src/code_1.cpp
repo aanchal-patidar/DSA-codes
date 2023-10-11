d6d5fdddca17e7c595effbcf4521c25456688283
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-11 21:42:37 +0530

    Update code_1.cpp

--- increment_decrement_codes_6/code_1/src/code_1.cpp
+++ increment_decrement_codes_6/code_1/src/code_1.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : code_1.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
