62452a516d3b0e5a4b6925398677c2ff8b66d503
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-13 19:16:19 +0530

    Update fibonacci_series.cpp

--- for_loop_7/fibonacci_series/src/fibonacci_series.cpp
+++ for_loop_7/fibonacci_series/src/fibonacci_series.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : fibonacci_series.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
