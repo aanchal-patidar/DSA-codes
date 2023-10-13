a6d533e3c45dd28c1738cbed687edb04ccc6aa23
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-13 19:18:25 +0530

    Update print_sum_1_to_n.cpp

--- for_loop_7/print_sum_1_to_n/src/print_sum_1_to_n.cpp
+++ for_loop_7/print_sum_1_to_n/src/print_sum_1_to_n.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : print_sum_1_to_n.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
