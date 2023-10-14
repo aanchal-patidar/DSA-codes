a84742339a4fd6078e465f3dea10bee52aea2508
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-14 20:51:23 +0530

    Update codes_2.cpp

--- for_loop_codes_8/codes_2/src/codes_2.cpp
+++ for_loop_codes_8/codes_2/src/codes_2.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : codes_2.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
