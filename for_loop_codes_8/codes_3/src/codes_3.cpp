35c02b67ed9058c3db567ac880d6fc2b130358d9
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-10-14 20:52:58 +0530

    Update codes_3.cpp

--- for_loop_codes_8/codes_3/src/codes_3.cpp
+++ for_loop_codes_8/codes_3/src/codes_3.cpp
@@ -1,8 +1,7 @@
 //============================================================================
 // Name        : codes_3.cpp
-// Author      : 
+// Author      : Aanchal Patidar
 // Version     :
-// Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
@@ -11,12 +10,12 @@ using namespace std;
 
 int main() {
 	for(int i = 0 ; i < 5 ; i++){
-			for(int j = i ; j <= 5 ; j++){
-				if(i+j == 10){
-					break;
-				}
-				cout << i << " " << j << endl;
+		for(int j = i ; j <= 5 ; j++){
+			if(i+j == 10){
+				break;
 			}
+			cout << i << " " << j << endl;
 		}
+	}
 	return 0;
 }
