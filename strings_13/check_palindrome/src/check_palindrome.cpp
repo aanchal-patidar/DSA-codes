f0112e212eb06e44f41eac62c77aac76ba6c48a6
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-12-02 19:07:38 +0530

    Update check_palindrome.cpp

--- strings_13/check_palindrome/src/check_palindrome.cpp
+++ strings_13/check_palindrome/src/check_palindrome.cpp
@@ -1,9 +1,7 @@
 //============================================================================
 // Name        : check_palindrome.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Author      : Aanchal Patidar
+// Description : 
 //============================================================================
 
 #include <iostream>
