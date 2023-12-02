5329c698332af0f7457912d16bdf25fee46901c0
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-12-02 18:51:41 +0530

    Update circular_insertion_head.cpp

--- linked_list_14/circular_insertion_head/src/circular_insertion_head.cpp
+++ linked_list_14/circular_insertion_head/src/circular_insertion_head.cpp
@@ -1,9 +1,7 @@
 //============================================================================
 // Name        : circular_insertion_head.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Author      : Aanchal Patidar
+// Description : 
 //============================================================================
 
 #include <iostream>
