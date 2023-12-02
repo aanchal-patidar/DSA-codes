6ec466a8a767eb8a8088db0eeb9c0dfec4eb8e10
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-12-02 18:55:52 +0530

    Update insertion_at_head.cpp

--- linked_list_14/insertion_at_head/src/insertion_at_head.cpp
+++ linked_list_14/insertion_at_head/src/insertion_at_head.cpp
@@ -1,9 +1,7 @@
 //============================================================================
 // Name        : insertion_at_head.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Author      : Aanchal Patidar
+// Description : 
 //============================================================================
 
 #include <iostream>
