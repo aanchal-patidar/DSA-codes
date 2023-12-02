93b9cefd85b3a002d3c7ae6ca9da9caada32ba92
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-12-02 18:54:34 +0530

    Update doubly_insertion_head_and_len.cpp

--- linked_list_14/doubly_insertion_head_and_len/src/doubly_insertion_head_and_len.cpp
+++ linked_list_14/doubly_insertion_head_and_len/src/doubly_insertion_head_and_len.cpp
@@ -1,9 +1,7 @@
 //============================================================================
 // Name        : doubly_insertion_head_and_len.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Author      : Aanchal Patidar
+// Description : 
 //============================================================================
 
 #include <iostream>
