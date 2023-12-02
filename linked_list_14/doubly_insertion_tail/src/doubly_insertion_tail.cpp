d1d5b3dc7f0643146dea452d9be358b34a75b6d1
Aanchal Patidar <146761680+aanchal-patidar@users.noreply.github.com>
2023-12-02 18:55:17 +0530

    Update doubly_insertion_tail.cpp

--- linked_list_14/doubly_insertion_tail/src/doubly_insertion_tail.cpp
+++ linked_list_14/doubly_insertion_tail/src/doubly_insertion_tail.cpp
@@ -1,9 +1,7 @@
 //============================================================================
 // Name        : doubly_insertion_tail.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Author      : Aanchal Patidar
+// Description : 
 //============================================================================
 
 #include <iostream>
